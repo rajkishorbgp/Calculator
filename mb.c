394f4532c98ef2f7b06f64ac8083b110c92934cc
Raj kishor <rajkishor6207@gmail.com>
2022-03-15 18:10:15 +0530

    first program of Git Hub

---
+++ mb.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+	int ch,c;
+    do
+    {    
+		
+		printf("\n\t--------page:- 1--------\n\n");
+		printf("\t1. Area\n");
+		printf("\t2. Age\n");
+		printf("\t3. BMI\n");
+		printf("\t4. Date\n");
+		printf("\t5. Data(B,KB,MB,GB,TB,PB)\n");
+		printf("\t6. Discount\n");
+		printf("\t7. Langth\n");
+		printf("\t8. Number_system(Binary,Octal,Decimal,Hexadecimal)\n");
+		printf("\t9. Speed\n");
+		printf("\t10. Standard( +,-,*,/ )\n");
+		printf("\t11. Tumperature\n");
+		printf("\t12. Time\n");
+		printf("\t13. Volum\n");
+		printf("\t14. Weight and mass\n");
+		printf("\t15. Exit\n");
+		printf("\nEnter the choice:-  ");
+        scanf("%d",&ch);
+		switch (ch)
+		{
+		    case 1:
+			
+			break;
+		
+		    default:
+			break;
+		}
+    }while(!(ch==15));
+}		
