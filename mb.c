04c2a6582b6d6235a0421dd0ce4b05ca7b7b66f8
Raj kishor <rajkishor6207@gmail.com>
2022-03-15 20:14:41 +0530

    case : -> update

--- mb.c
+++ mb.c
@@ -25,12 +25,95 @@ void main()
         scanf("%d",&ch);
 		switch (ch)
 		{
-		    case 1:
-			
+		  case 1:
+		       {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+                  printf("\t_______________________Area________________________\n");			
+			   }
+			  break;
+          case 2:
+		       {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_______________________Age_________________________\n");
+			   }
 			break;
-		
-		    default:
-			break;
+		  case 3:
+		       {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_______________________BMI_________________________\n");
+			   }
+			  break;
+		  case 4:
+				{
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				    printf("\t_____________________Date________________________\n");
+			   }	
+		      break;
+		  case 5:
+		        {
+					printf("\t--------------Welcome to page:- 2 -----------------\n");
+				    printf("\t_____________________Date________________________\n");
+			    }	
+			  break;
+		  case 6:
+      		   {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_____________________Discount______________________\n");
+			   }
+		      break;
+		  case 7:
+               {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+			      printf("\t_____________________Langth________________________\n");
+			   }
+		      break;
+		  case 8:
+		       {
+				 printf("\t--------------Welcome to page:- 2 -----------------\n");
+				    printf("\t_____________________Date________________________\n");
+			   }	
+			   break;
+		  case 9:
+	        	{
+                   printf("\t--------------Welcome to page:- 2 -----------------\n");
+				   printf("\t_______________________Speed________________________\n");
+		        }
+		      break;    
+		  case 10:
+	         	{
+		          printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_____________________Date________________________\n");
+			    }
+		      break;
+	       case 11:
+               {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t___________________Tumperature_____________________\n");
+		       }
+	          break;
+	       case 12:
+	           {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_______________________Time________________________\n");
+			   }
+	          break;
+	       case 13:
+	           {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t______________________Volum________________________\n");
+			   }
+	          break;
+	       case 14:
+	           {
+				  printf("\t--------------Welcome to page:- 2 -----------------\n");
+				  printf("\t_________________Weight and mass___________________\n");
+			   }
+	          break;
+	       case 15:
+	          break;
+	       default:
+	               printf("\n\t......invalid input..........\n\tPlease choose the correct option...\n");
+             break;
 		}
     }while(!(ch==15));
 }		
